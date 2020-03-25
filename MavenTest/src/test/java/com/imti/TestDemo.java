51a02a764debd6ee29f0ea8e58f8041fa795db7d
lg5689134 <451082729@qq.com>
2020-03-25 16:46:19 +0800

    新增文件

---
+++ MavenTest/src/test/java/com/imti/TestDemo.java
@@ -0,0 +1,23 @@
+/**
+ * @filename UNITDemo.java
+ * @author lg
+ * @date 2020年2月21日 下午5:14:35
+ * @version 1.0
+ * Copyright (C) 2020 
+ */
+
+package com.imti;
+
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Test
+	public void aa(){
+		System.out.println("aaaaaaaaa-----------------");
+	}
+	@Test
+	public void bb(){
+		System.out.println("bbbbbbbbbb-================");
+	}
+}
